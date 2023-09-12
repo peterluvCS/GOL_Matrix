a4acad332b4dd770272441bd9231440dbb225ddf
peterluvCS <853571586@qq.com>
2023-09-12 19:34:08 +0800

    final

--- gol_matrix.c
+++ gol_matrix.c
@@ -1,47 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printMatrix(unsigned int *matrix) {
-    for (int i = 0; i < 5; i++){
-        for (int j = 0; j < 5; j++){
-            printf("%d", *(matrix + 5 * i + j - 1));
+struct _point {
+    unsigned int sum;
+    char state;
+};
+typedef struct _point point;
+
+void printMatrix(point *matrix) {
+    for (int i = 1; i <= 5; i++){
+        for (int j = 1; j <= 5; j++){
+            printf("%c ", (matrix + 7 * i + j)->state);
         }
         printf("\n");
     }
 } 
 
-unsigned int countNeighbors(char array[5][5],int row, int col) {
-    char * ptr = &array[0][0];
-    ptr = ptr + row * 5 + col;
+void countNeighbors(point *matrix) {
+    point *ptr = matrix;
+    //ptr = ptr + row * 7 + col;
 
-    unsigned int sum = 0;
-    if (*(ptr+1) == '*') sum++;
-    if (*(ptr-1) == '*') sum++;
-    if (*(ptr+5) == '*') sum++;
-    if (*(ptr-5) == '*') sum++;
-    if (*(ptr+4) == '*') sum++;
-    if (*(ptr+6) == '*') sum++;
-    if (*(ptr-4) == '*') sum++;
-    if (*(ptr-6) == '*') sum++;
+    
+    if ((ptr+1)->state == '*') ptr->sum++;
+    if ((ptr-1)->state == '*') ptr->sum++;
+    if ((ptr+6)->state == '*') ptr->sum++;
+    if ((ptr-6)->state == '*') ptr->sum++;
+    if ((ptr+7)->state == '*') ptr->sum++;
+    if ((ptr-7)->state == '*') ptr->sum++;
+    if ((ptr+8)->state == '*') ptr->sum++;
+    if ((ptr-8)->state == '*') ptr->sum++;
 
-    return sum;
+    //return sum;
+}
+
+void changeState(point *matrix){
+    point *ptr = matrix;
+
+    if (ptr->state == '*') {
+        if (ptr->sum < 2 || ptr->sum > 3) {
+            ptr->state = ' ';
+        } else {
+            ptr->state = '*';
+        }
+    } else if (ptr->state == ' ') {
+        if (ptr->sum < 3 || ptr->sum > 3) {
+            ptr->state = ' ';
+        } else {
+            ptr->state = '*';
+        }
+    }
 }
 
 int main() {
     
-    unsigned int matrix[5][5];
-    unsigned int *ptr = matrix;
-    for (int i = 0; i < 5; i++){{}
-        for (int j = 0; j < 5; j++) {
-            if (i == 2){
-                matrix[i][j] = 1;
-            } else {
-                matrix[i][j] = 0;
-            }           
+    point matrix[7][7];
+
+    point defaultPoint;
+    defaultPoint.state = ' ';
+    defaultPoint.sum = 0;
+
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 7; j++) {
+            matrix[i][j] = defaultPoint;
         }
     }
 
-    printf("%d\n", countNeighbors(matrix, 2, 2));
+    point *ptr = matrix;
+
+    for (int j = 1; j <= 5; j++){       
+        (ptr + 3 * 7 + j)->state = '*';
+        //matrix[3][j]->state = '*';        
+    }
+    // printMatrix(matrix);
+    //unsigned int (*ptr)[7] = matrix;
+    //printMatrix(matrix);
+    
+    point *element;
+    for (int row = 1; row <= 5; row++) {
+        for (int col = 1; col <= 5; col++) {
+            element = &matrix[row][col];
+            countNeighbors(element);
+        }
+    }
+
+    for (int row = 1; row <= 5; row++) {
+        for (int col = 1; col <= 5; col++) {
+            element = &matrix[row][col];
+            changeState(element);
+        }
+    }
+
+    printMatrix(matrix);
+    
     
 
     return EXIT_SUCCESS;
